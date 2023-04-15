008765c3237701ad45d4faad2c185dab992b634a
Muditgoyal-FED <121019540+Muditgoyal-FED@users.noreply.github.com>
2023-04-16 01:27:11 +0530

    Add files via upload

--- image slider/script.js
+++ image slider/script.js
@@ -1,23 +1,23 @@
-let flag = 0;
+let image = 0;
 
 
 function controller(x){
-     flag = flag + x;
-     slideshow(flag);
+     image = image + x;
+     slideshow(image);
 }
 
 
-slideshow(flag);
+slideshow(image);
 
 function slideshow(num){
   let slides = document.getElementsByClassName('slide');
 
   if(num == slides.length){
-    flag = 0;
+    image = 0;
     num = 0;
   }
   if (num < 0 ){
-    flag = slides.length-1;
+    image = slides.length-1;
     num = slides.length-1;
   }
 
